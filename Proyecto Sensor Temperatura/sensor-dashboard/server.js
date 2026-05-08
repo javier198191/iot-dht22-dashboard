54c8df6a55981dd736f364d791ecff1c8c1fd81a
javier198191 <javierlasprilla1@gmail.com>
2026-05-07 23:05:15 -0500

    modificando las peticiones

--- Proyecto Sensor Temperatura/sensor-dashboard/server.js
+++ Proyecto Sensor Temperatura/sensor-dashboard/server.js
@@ -19,6 +19,7 @@ app.use('/api/sensor', require('./routes/sensor'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/dashboard', (req, res) => res.sendFile(path.join(__dirname, 'public', 'dashboard.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'public', 'about.html')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
